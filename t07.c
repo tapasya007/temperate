2a66cfc260154c5ad86b26d84afc311ce5ff6c19
tapasya007 <130670150+tapasya007@users.noreply.github.com>
2023-04-26 17:54:17 +0530

    Add files via upload

---
+++ t07.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main()
+{
+	int n,num,rev=0;
+	
+	printf("enter any number to check palindrome:");
+	scanf("%d",&n);
+	
+	num=n;
+	
+	while(n!=0)
+	{
+		rev=(rev*10)+(n%10);
+		n/=10;
+	}
+	
+	if(rev==num)
+	{
+		printf("%d is palindrome.",num);
+	}
+	else
+	{
+		printf("%d is not palindrome.",num);
+	}
+}
